3bcaf0d27f570892bbcd4a5d09c64cf6d8f81814
Manas Mahajan <manas.mahajan99@gmail.com>
2024-08-19 23:33:59 +0530

    added connection pooling

--- configs/db.py
+++ configs/db.py
@@ -1,8 +1,31 @@
-import os
 import psycopg2
+from psycopg2 import pool
+import os
 
 DATABASE_URI = os.getenv('DATABASE')
 
+
+connection_pool = None
+
+def init_connection_pool(minconn=1, maxconn=10):
+    global connection_pool
+    connection_pool = psycopg2.pool.SimpleConnectionPool(minconn, maxconn, DATABASE_URI)
+
 def get_db_connection():
-    conn = psycopg2.connect(DATABASE_URI)
-    return conn
+    """
+    Returns a database connection from the pool.
+    """
+    if connection_pool:
+        return connection_pool.getconn()
+    else:
+        # Fall back to a single connection if no pool is used
+        return psycopg2.connect(DATABASE_URI)
+
+def release_db_connection(conn):
+    """
+    Releases the database connection back to the pool.
+    """
+    if connection_pool:
+        connection_pool.putconn(conn)
+    else:
+        conn.close()
